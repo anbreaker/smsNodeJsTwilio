dd3ffa176817d1e70ff3c4b073392c7ac43f9f94
anbreaker <antunez19@gmail.com>
2020-06-11 19:21:19 +0200

    send sms done

--- src/libs/resources.handlebars.js
+++ src/libs/resources.handlebars.js
@@ -1,6 +1,12 @@
+const timeago = require('timeago.js');
+
 module.exports = {
   //+346321456778 --> +xxxxxxxxx
   hideNumber: (phoneNumber = '') => {
     return phoneNumber.replace(/[0-9]/g, 'x');
+  },
+
+  timeago: (data) => {
+    return timeago.format(data);
   },
 };
--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,7 +1,41 @@
 const socket = io();
 
+// https://developer.mozilla.org/es/docs/Web/API/notification
+
+Notification.requestPermission().then(function (result) {
+  console.log(result);
+});
+
+function notifyMe(sms = 'Hi there!') {
+  // Let's check if the browser supports notifications
+  if (!('Notification' in window)) {
+    alert('This browser does not support desktop notification');
+  }
+
+  // Let's check whether notification permissions have already been granted
+  else if (Notification.permission === 'granted') {
+    // If it's okay let's create a notification
+    var notification = new Notification(sms);
+  }
+
+  // Otherwise, we need to ask the user for permission
+  else if (Notification.permission !== 'denied') {
+    Notification.requestPermission(function (permission) {
+      // If the user accepts, let's create a notification
+      if (permission === 'granted') {
+        var notification = new Notification(sms);
+      }
+    });
+  }
+
+  // At last, if the user has denied notifications, and you
+  // want to be respectful there is no need to bother them any more.
+}
+
 socket.on('new sms', (data) => {
   console.log('Nuevo SmS');
+
+  notifyMe('New sms received');
 
   const smsList = document.getElementById('sms');
 
@@ -14,7 +48,16 @@ socket.on('new sms', (data) => {
   const from = document.createElement('span');
   from.appendChild(document.createTextNode(data.From));
 
+  const _id = document.createElement('span');
+  _id.appendChild(document.createTextNode(data._id));
+
+  const createdAt = document.createElement('span');
+  createdAt.appendChild(document.createTextNode(timeago.format(data.createdAt)));
+
   li.appendChild(paragraph);
+  li.appendChild(_id);
   li.appendChild(from);
+  li.appendChild(createdAt);
+
   smsList.prepend(li);
 });
